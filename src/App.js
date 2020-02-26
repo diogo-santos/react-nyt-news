24099087eda2cae43dbb3eff953ec347e3779b1d
diogo-santos <diogoffs@outlook.com>
2020-02-26 17:08:03 +0000

    Rendering simple front page for the stories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    news: []
+  }
+  render() {
+    return (
+       <div className="container">
+        <div className="col-xs-12">
+        <h1>NYT News - Top Stories</h1>
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Post title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">
+                <span>Post abstract</span>
+              </h6>
+            </div>
+          </div>
+        </div>
+       </div>
+    );
+  }
 }
 
 export default App;
