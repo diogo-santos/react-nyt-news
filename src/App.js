a70251ccb640322945eb8aed7b62395c0659ab3a
diogo-santos <diogoffs@outlook.com>
2020-02-26 17:23:45 +0000

    Adding news count

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    news: []
+    news: [],
+    newsCount: 0
   }
   componentDidMount() {
     fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=usMFEIhDlH6rz0pdz0YATYU7LShy3XJB')
     .then(response => response.json())
     .then((data) => {
       this.setState({news: data.results})
+      this.setState({
+        news: data.results,
+        newsCount: data.num_results
+      })
     })
   }
   render() {
     return (
-       <div className="container">
+      <div className="container">
         <div className="col-xs-12">
-        <h1>NYT News - Top Stories</h1>
+    <h1>NYT News - Top Stories ({this.state.newsCount})</h1>
           { this.state.news.map((post) => (
             <div className="card">
               <div className="card-body">
@@ -27,7 +32,7 @@ class App extends Component {
             </div>
           ))}
         </div>
-       </div>
+      </div>
     );
   }
 }
