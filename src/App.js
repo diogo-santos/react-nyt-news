b87437c45f4abc8a16dd499fd16af7993bcdfb1f
diogo-santos <diogoffs@outlook.com>
2020-02-26 17:28:46 +0000

    Adding url to title

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ class App extends Component {
           { this.state.news.map((post) => (
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">{post.title}</h5>
+                <h5 className="card-title">
+                <a href={post.url} target="_blank">{post.title}</a>
+                </h5>
                 <h6 className="card-subtitle mb-2 text-muted">
                   <span>{post.abstract}</span>
                 </h6>
