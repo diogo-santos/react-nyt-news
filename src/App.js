6491efa63427307ff3c8018e4ac53ec921284ed4
diogo-santos <diogoffs@outlook.com>
2020-02-26 17:11:32 +0000

    Fetching data from Top Stories API

--- src/App.js
+++ src/App.js
@@ -4,19 +4,28 @@ class App extends Component {
   state = {
     news: []
   }
+  componentDidMount() {
+    fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=usMFEIhDlH6rz0pdz0YATYU7LShy3XJB')
+    .then(response => response.json())
+    .then((data) => {
+      this.setState({news: data.results})
+    })
+  }
   render() {
     return (
        <div className="container">
         <div className="col-xs-12">
         <h1>NYT News - Top Stories</h1>
-          <div className="card">
-            <div className="card-body">
-              <h5 className="card-title">Post title</h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                <span>Post abstract</span>
-              </h6>
+          { this.state.news.map((post) => (
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">{post.title}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">
+                  <span>{post.abstract}</span>
+                </h6>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
        </div>
     );
