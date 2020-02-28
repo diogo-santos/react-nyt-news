3ddfc48ecfea6db288f23bd6124c23a5d79fb0e8
diogo-santos <diogoffs@outlook.com>
2020-02-28 23:48:51 +0000

    Add image to the post with link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   state = {
@@ -7,29 +8,28 @@ class App extends Component {
   }
   componentDidMount() {
     fetch('https://api.nytimes.com/svc/topstories/v2/home.json?api-key=usMFEIhDlH6rz0pdz0YATYU7LShy3XJB')
-    .then(response => response.json())
-    .then((data) => {
-      this.setState({news: data.results})
-      this.setState({
-        news: data.results,
-        newsCount: data.num_results
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({ news: data.results })
+        this.setState({
+          news: data.results,
+          newsCount: data.num_results
+        })
       })
-    })
   }
   render() {
     return (
       <div className="container">
-        <div className="col-xs-12">
-    <h1>NYT News - Top Stories ({this.state.newsCount})</h1>
-          { this.state.news.map((post) => (
-            <div className="card">
+        <h1>NYT News - Top Stories ({this.state.newsCount})</h1>
+        <div className="row">
+          {this.state.news.map((post) => (
+            <div className="card" style={{ width: 18 + 'rem' }}>
+              <a href={post.url} target="_blank" rel="noopener noreferrer">
+                <img className="card-img-top" src={post.multimedia[0].url} alt={post.caption} />
+              </a>
               <div className="card-body">
-                <h5 className="card-title">
-                <a href={post.url} target="_blank">{post.title}</a>
-                </h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  <span>{post.abstract}</span>
-                </h6>
+                <h5 className="card-title">{post.title}</h5>
+                <p className="card-text text-muted">{post.abstract}</p>
               </div>
             </div>
           ))}
