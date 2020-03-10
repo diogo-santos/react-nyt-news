3790969bc07b0e5867cbef43fbd78e6a491aa8cf
diogo-santos <diogoffs@outlook.com>
2020-03-10 00:05:58 +0000

    Rename news to post variable

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const API_KEY = process.env.REACT_APP_NYT_API_KEY;
 
 class App extends Component {
   state = {
-    news: [],
-    newsCount: 0
+    posts: [],
+    postsCount: 0
   }
   handleErrors(response) {
     if (!response.ok) {
@@ -22,10 +22,10 @@ class App extends Component {
       .then(this.handleErrors)
       .then(response => response.json())
       .then((data) => {
-        this.setState({ news: data.results })
+        this.setState({ posts: data.results })
         this.setState({
-          news: data.results,
-          newsCount: data.num_results
+          posts: data.results,
+          postCount: data.num_results
         })
       })
       .catch(error => console.log(error));
@@ -33,8 +33,8 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>NYT News - Top Stories ({this.state.newsCount})</h1>
-        <PostList posts={this.state.news} />
+        <h1>NYT News - Top Stories ({this.state.postCount})</h1>
+        <PostList posts={this.state.posts} />
       </div>
     );
   }
