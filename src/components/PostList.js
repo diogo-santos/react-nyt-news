2b0bcfd0c862f4c8a9d5a2184182069aa91a5931
diogo-santos <diogoffs@outlook.com>
2020-03-13 21:29:08 +0000

    Fixing card columns

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,18 +3,20 @@ import React from "react";
 
 function PostList(props) {
   return (
-    <div className="row card-columns">
-    {props.posts.map((post, i) => (
-      <div className="card" key={i} style={{ width: 18 + 'rem' }}>
-        <a href={post.url} target="_blank" rel="noopener noreferrer">
-          <img className="card-img-top" src={post.multimedia && post.multimedia.length >= 1? post.multimedia[0].url : ""} alt="" />
-        </a>
-        <div className="card-body">
-          <h5 className="card-title">{post.title}</h5>
-          <p className="card-text text-muted">{post.abstract}</p>
+    <div className="row">
+      <div className="card-columns">
+      {props.posts.map((post, i) => (
+        <div className="card" key={i} style={{ width: 18 + 'rem' }}>
+          <a href={post.url} target="_blank" rel="noopener noreferrer">
+            <img className="card-img-top" src={post.multimedia && post.multimedia.length >= 1? post.multimedia[0].url : ""} alt="" />
+          </a>
+          <div className="card-body">
+            <h5 className="card-title">{post.title}</h5>
+            <p className="card-text text-muted">{post.abstract}</p>
+          </div>
         </div>
-      </div>
-    ))}
+      ))}
+    </div>
   </div>
   );
 } 
